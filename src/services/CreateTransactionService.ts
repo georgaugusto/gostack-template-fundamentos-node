70b5db48feb0f5f3c21b776ddfc4048071ae047a
Georg Augusto Schlegel <georgaugusto@gmail.com>
2020-04-24 22:37:09 -0300

    challenge concluded

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    if ((type === 'income' || type === 'outcome') && value <= 0) {
+      throw Error('You must deposit a value greater than zero');
+    }
+
+    if (type === 'outcome' && (balance.total < value || value === 0)) {
+      throw Error('You don`t have enough balance');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
